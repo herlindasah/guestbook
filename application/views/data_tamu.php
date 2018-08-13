7a3f495eafd8097f3543b4cf0683a8f54f59e341
Erik Santiago <eriksantiago.science@gmail.com>
2018-08-13 15:03:45 +0700

    add icon on nvbar collapse button

--- application/views/data_tamu.php
+++ application/views/data_tamu.php
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-inverse fixed-top">
         <a class="navbar-brand text-white" href="<?php echo base_url() ?>dashboard">Buku Tamu PGN COM</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"><i class="fa fa-bars text-white"></i></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav my-lg-0 ml-auto">
